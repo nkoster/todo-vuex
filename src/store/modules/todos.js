42c7313fc01c470a3c2b9573f835aa86d5ad88c5
Niels Koster <n.koster@portavita.eu>
2019-04-22 19:22:48 +0200

    Add fetchTodos.

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,29 +1,30 @@
-//import axios from 'axios'
-
+import axios from 'axios'
+/* eslint-disable */
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: 'One'
-        },
-        {
-            id: 2,
-            title: 'Two'
-        }
-    ]
+    todos: []
 }
 
 const getters = {
     allTodos: state => state.todos
 }
 
-const action = {}
+const actions = {
+    async fetchTodos({ commit }) {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+        commit('setTodos', response.data)
+    },
+    async addTodo() {
+        
+    }
+}
 
-const mutations = {}
+const mutations = {
+    setTodos: (state, todos) => (state.todos = todos)
+}
 
 export default {
     state,
     getters,
-    action,
+    actions,
     mutations
 }
