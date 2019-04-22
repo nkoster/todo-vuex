f18e59a3e86684c6a5b01128ac6e6f3e71268467
Niels Koster <n.koster@portavita.eu>
2019-04-23 01:02:18 +0200

    Add deleteTodo and filterTodos

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+/* eslint-disable */
+const uuidv1 = require('uuid/v1');
 
 const state = {
     todos: []
@@ -17,16 +19,29 @@ const actions = {
     async addTodo({ commit }, title) {
         const response = 
             await axios.post('http://my-json-server.typicode.com/typicode/demo/posts', {
+                id: uuidv1(),
                 title,
                 completed: false
             });
         commit('newTodo', response.data)
+    },
+    async deleteTodo({ commit }, id) {
+        // await axios.delete(`http://my-json-server.typicode.com/typicode/demo/posts/${id}`)
+        //     .catch();
+        commit('removeTodo', id)
+    },
+    async filterTodos({ commit }, e) {
+        const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText)
+        const response = 
+            await axios.get(`http://my-json-server.typicode.com/typicode/demo/posts?_limit=${limit}`)
+        commit('setTodos', response.data)
     }
 }
 
 const mutations = {
     setTodos: (state, todos) => (state.todos = todos),
-    newTodo: (state, todo) => state.todos.unshift(todo)
+    newTodo: (state, todo) => state.todos.unshift(todo),
+    removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id)
 }
 
 export default {
