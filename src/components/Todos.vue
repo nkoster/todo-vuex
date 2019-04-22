9ca3a2861749ab79634505d985ac7d9dca2ddf8e
Niels Koster <n.koster@portavita.eu>
2019-04-23 01:22:05 +0200

    Add legend plus cool CSS stuff.

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
         <h3>Todos</h3>
+        <div class="legend">
+            <span>Double click when complete</span>
+            <span>
+                <span class="incomplete-box"></span> = Incomplete
+            </span>
+            <span>
+                <span class="complete-box"></span> = Complete
+            </span>
+        </div>
         <div class="todos">
             <div v-for="todo in allTodos" :key="todo.id" class="todo">
                 {{todo.title}}
@@ -33,7 +42,8 @@ export default {
 }
 .todo {
     border: 1px solid #ccc;
-    background: #41b883;
+    background: #35495e;
+    color: #fff;
     padding: 1rem;
     border-radius: 15px 15px 3px;
     text-align: center;
@@ -46,5 +56,27 @@ i {
     right: 10px;
     color: #fff;
     cursor: pointer;
+}
+.legend {
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 1rem;
+}
+.complete-box {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background: #41b883;
+}
+.incomplete-box {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background: #35495e;
+}
+@media (max-width: 500px) {
+    .todos {
+        grid-template-columns: 1fr;
+    }
 }
 </style>
